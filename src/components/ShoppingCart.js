403ab8029035ce6598ea64120412981172719765
ElhamNawabi <nawabi.elham@gmail.com>
2023-04-21 18:44:16 -0400

    first commit

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+const ShoppingCart = () => {
+  return (
+    <div className="App-header">
+        test
+      </div>
+  )
+}
+
+export default ShoppingCart
